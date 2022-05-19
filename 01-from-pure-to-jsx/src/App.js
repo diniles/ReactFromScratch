0e049058eabe18cfb0b0494447d12eaf04190dfd
Dzmitry Liashko <dzliashko@gmail.com>
2022-05-19 23:04:01 +0300

    Add: conditional drawing

--- 01-from-pure-to-jsx/src/App.js
+++ 01-from-pure-to-jsx/src/App.js
@@ -6,6 +6,7 @@ const App = () => {
       <Book name="JS for beginners" year="2018" price="1000">Children props</Book>
       <Book name="React for beginners" year="2019" price="1200"/>
       <Book name="Vue for beginners" year="2021" price="1500"/>
+      <Book name='' year="2021" price="1500"/>
     </div>
   )
 };
--- 01-from-pure-to-jsx/src/Book.js
+++ 01-from-pure-to-jsx/src/Book.js
@@ -1,9 +1,12 @@
 import React from 'react';
 
 const Book = (props) => {
+  // if (!props.name) {
+  //   return null;
+  // }
   return (
     <div>
-      <h2>{props.name}</h2>
+      <h2>{props.name ? props.name : 'No Name'}</h2>
       <p>{props.year}</p>
       <p>{props.price}</p>
       <p>{props.children}</p>
