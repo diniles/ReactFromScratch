0b13a8eacef8945cfd9b02716719a84c551d5cf7
Dzmitry Liashko <dzliashko@gmail.com>
2022-05-19 22:41:03 +0300

    Add: convert to JSX

---
+++ 01-from-pure-to-jsx/src/Book.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Book = (props) => {
+  // return React.createElement("div", {}, [
+  //   React.createElement("h2", {}, props.name),
+  //   React.createElement("p", {}, props.year),
+  //   React.createElement("p", {}, props.price)
+  // ]);
+  return (
+    <div>
+      <h2>{props.name}</h2>
+      <p>{props.year}</p>
+      <p>{props.price}</p>
+    </div>
+  )
+};
+
+export default Book;
