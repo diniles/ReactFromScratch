64ad9d9c8afe30fd1fed37d64427cb0e0ed94e16
Dzmitry Liashko <dzliashko@gmail.com>
2022-05-21 22:12:44 +0300

    Reactor: most common used scenario

--- cra/src/App.js
+++ cra/src/App.js
@@ -8,9 +8,10 @@ class App extends Component {
   };
 
   handleClick = () => {
-    // this.setState({count: this.state.count + 1})
+    // most common used scenario
+    this.setState({count: this.state.count + 1})
     // to change state more than once do this
-    this.setState((prevState) => ({count: prevState.count + 1}), () => console.log('setState Complete'));
+    // this.setState((prevState) => ({count: prevState.count + 1}), () => console.log('setState Complete'));
     // this.setState((prevState) => ({count: prevState.count + 1}));
     // this.setState((prevState) => ({count: prevState.count + 1}));
     console.log('from handleClick');
