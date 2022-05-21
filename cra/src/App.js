1987515b3ee40a6c94753cd0578eab7155810637
Dzmitry Liashko <dzliashko@gmail.com>
2022-05-21 22:11:19 +0300

    Reactor: return correct code to previous commit

--- cra/src/App.js
+++ cra/src/App.js
@@ -11,6 +11,8 @@ class App extends Component {
     // this.setState({count: this.state.count + 1})
     // to change state more than once do this
     this.setState((prevState) => ({count: prevState.count + 1}), () => console.log('setState Complete'));
+    // this.setState((prevState) => ({count: prevState.count + 1}));
+    // this.setState((prevState) => ({count: prevState.count + 1}));
     console.log('from handleClick');
   }
 
