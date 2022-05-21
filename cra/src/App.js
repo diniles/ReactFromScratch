951031e8b913df5e826120ff4bb5b458836a4497
Dzmitry Liashko <dzliashko@gmail.com>
2022-05-21 22:59:06 +0300

    Refactor: test solution works, but not passed

--- cra/src/App.js
+++ cra/src/App.js
@@ -3,31 +3,21 @@ import React, {Component} from "react";
 
 // class App extends React.Component {
 class App extends Component {
-  state = {
-    count: 0
+  constructor() {
+    super();
+    this.state = {
+      count: 0,
+    };
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    this.setState({count: this.state.count + 1});
   };
-
-  // handleClick = (sign) => {
-  //   // most common used scenario
-  //   // this.setState({count: this.state.count + 1})
-  //   // to change state more than once do this
-  //   // this.setState((prevState) => ({count: prevState.count + 1}), () => console.log('setState Complete'));
-  //   // this.setState((prevState) => ({count: prevState.count + 1}));
-  //   // this.setState((prevState) => ({count: prevState.count + 1}));
-  //   // console.log('from handleClick');
-  // }
-
-  // handleClickPlus = () => {
-  //   this.setState({count: this.state.count + 1})
-  // }
-  // handleClickMinus = () => {
-  //   this.setState({count: this.state.count - 1})
-  // }
 
   render() {
     return (
       <div className="App">
-        Common React Application
         <button onClick={() => this.setState({count: this.state.count + 1})}>+</button>
         <span>{this.state.count}</span>
         <button onClick={() => this.setState({count: this.state.count - 1})}>-</button>
