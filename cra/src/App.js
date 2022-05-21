7075013cd6e7f443db4ff1bf9cf1b38f1e062c1e
Dzmitry Liashko <dzliashko@gmail.com>
2022-05-21 22:03:04 +0300

    Refactor: remove constructor

--- cra/src/App.js
+++ cra/src/App.js
@@ -3,12 +3,9 @@ import React, {Component} from "react";
 
 // class App extends React.Component {
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      count: 0
-    };
-  }
+  state = {
+    count: 0
+  };
 
   handleClick = () => {
     this.setState({count: this.state.count + 1})
