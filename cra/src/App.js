6f59a31fdf1da10789f24ca78c300d5597d2cd96
Dzmitry Liashko <dzliashko@gmail.com>
2022-05-21 22:01:49 +0300

    Add: handler to state

--- cra/src/App.js
+++ cra/src/App.js
@@ -7,14 +7,18 @@ class App extends Component {
     super(props);
     this.state = {
       count: 0
-    }
+    };
+  }
+
+  handleClick = () => {
+    this.setState({count: this.state.count + 1})
   }
 
   render() {
     return (
       <div className="App">
-        <h1>Common React Application</h1>
-        <button>{this.state.count}</button>
+        Common React Application
+        <button onClick={this.handleClick}>{this.state.count}</button>
       </div>
     );
   }
