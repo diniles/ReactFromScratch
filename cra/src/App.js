f853c605ec5727e6297ef398308cf3a81799f5f8
Dzmitry Liashko <dzliashko@gmail.com>
2022-05-21 21:48:40 +0300

    Refactor: function to class

--- cra/src/App.js
+++ cra/src/App.js
@@ -1,9 +1,17 @@
-function App() {
-  return (
-    <div className="App">
-      <h1>Common React Application</h1>
-    </div>
-  );
+import React from "react";
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Common React Application</h1>
+      </div>
+    );
+  }
 }
 
 export default App;
